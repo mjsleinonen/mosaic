0b463d548d5dc5636f96c003e41852001eec2856
mjleinon <37039101+mjleinon@users.noreply.github.com>
2021-03-06 13:56:37 +0200

    Create example.py

---
+++ example.py
@@ -0,0 +1,26 @@
+from mosaic import *
+
+t = Tiles()
+print("initializing images")
+#pathends = ["one","two","three","four","five"]
+        
+t.get_pictures_from_array(dat[:1800,:])
+            
+path_to_tile_image = "..."
+        
+print("clustering images")
+t.tile_image(path_to_tile_image)
+t.cluster_up(20) #
+        
+print("mosaic creation bitches")
+        
+t.create_mosaic_image(closer_to_clust=True,
+                              blend_original=True,
+                              blend_ratio=0.30)
+
+import os
+path = os.path.abspath(a_module.__file__)
+
+path = path+meitsimosaic{}.jpg".format(int(time.time()))
+        
+to_image(t.mosaic,path)

e562b88af153e1d0eac9ed0ac16983fabdd407d4
mjsleinonen <mikkoleinonen@gmail.com>
2024-03-30 16:27:08 +0200

    Make gui nicer...

--- imagetiler.py
+++ imagetiler.py
@@ -12,6 +12,10 @@ from random import randint
 import tkinter as tk
 from tkinter import ttk
 
+import tkinter.font as font
+
+from pathlib import Path
+
 import time
 
 from PIL import Image, ImageTk
@@ -21,6 +25,18 @@ from sklearn.cluster import KMeans
 
 import numpy as np
 import os
+
+import contextlib
+
+@contextlib.contextmanager
+def visit_dir(dirname):
+    curdir = os.getcwd()
+    
+    os.chdir(dirname)
+    try:
+        yield
+    finally:
+        os.chdir(curdir)
 
 def to_array(photo):
     I = np.asarray(photo)
@@ -49,26 +65,26 @@ def row_to_image(row, check=True):
     pic = np.array([color for color in (r,g,b)])
     pic = np.moveaxis(pic, 0, -1)
     
-    return(pic)
+    return pic
 
 def distance(vec1,vec2):
     return(np.sqrt(np.sum((vec1-vec2)**2)))
     
 def get_filename_dialog(root, initdir="/"):
     root.filename =  filedialog.askopenfilename(initialdir=initdir,title="Select file",filetypes=(("jpeg files","*.jpg"),("all files","*.*")))
-    return(root.filename)
+    return root.filename
     
 def get_foldername_dialog(root, initdir="/"):
     root.foldername =  filedialog.askdirectory(initialdir=initdir,title="Select file")
-    return(root.foldername)
+    return root.foldername
     
 def get_files_dir():
     files_path = [os.path.abspath(x) for x in os.listdir()]
-    return(files_path)
+    return files_path
     
 def get_files_dir_path(path):
     files_path = [os.path.abspath(x) for x in os.listdir()]
-    return(files_path)
+    return files_path
     
 def kmeans_model(arr,axis=2,n_clust=20):
     
@@ -76,7 +92,7 @@ def kmeans_model(arr,axis=2,n_clust=20):
 
     vectors = arr.reshape(nx*ny,3)
     pkmeans = KMeans(n_clusters=n_clust, random_state=0).fit(vectors)
-    return(pkmeans)
+    return pkmeans
     
 def array_stats_(arr):
     stats = []
@@ -91,7 +107,7 @@ def array_stats(arr):
     vec = [np.bincount(arr[:,:,i].flatten()).argmax() for i in range(3)]
     stats.append(vec)
 
-    return(stats)
+    return stats
     
 def towards_mean(arr):
     meancolor = np.array([np.mean(arr[:,:,i]) for i in range(3)])
@@ -104,7 +120,15 @@ def towards_mean(arr):
         arr2 += meancolor[i]
         arr2 /= 2
         
-    return(arr2)
+    return arr2
+
+def ext_check(name, exts):
+    return name.split(".")[-1] in exts
+
+def timestamp():
+    return int(time.time())
+
+EXTS = ["jpg","jpeg","png","bmp","tif"]
     
 class Gui1:
     
@@ -128,9 +152,11 @@ class Gui1:
         self.filemenu.add_command(label ='Exit', command = root.destroy) 
         
         self.root.config(menu = self.menubar)
-        
-        "-- gui variables "
 
+        "-- gui variables "
+        
+        fontti = font.Font(size=15)
+    
         self.imagespath = StringVar()
         cf = os.getcwd()
         filename = cf+"\\"
@@ -144,86 +170,155 @@ class Gui1:
          
         "-- first tab and sub widgets"
         
-        self.tabs = ttk.Notebook(root, width=self.fdx, height=self.fdy)
+        #self.tabs = ttk.Notebook(root, width=self.fdx, height=self.fdy)
+        self.main = Frame(root, highlightbackground="grey", highlightthickness=1)
+        self.main.pack(fill="both")
         
-        f1 = Frame(self.tabs)
-        self.tabs.add(f1, text="Create Mosaic Image")
+        f1 = Frame(self.main, highlightbackground="grey", highlightthickness=1)
+        #self.tabs.add(f1, text="Create Mosaic Image")
+        f1.pack()
         
-        f2 = Frame(f1)
-        f2.pack()
+        #f2 = Frame(self.main, highlightbackground="grey", highlightthickness=1)
+        #f2.pack()
         
-        self.filename = Label(f2, textvariable=self.imagespath, width=100)
-        self.filename.pack(side=RIGHT)
-        
-        bbtn1 = Button(f2, width=15, text="Add Folder", command=self.add_folder_to_scheme)
-        bbtn1.pack(side=LEFT)
-        
-        f3 = Frame(f1)
+        f3 = Frame(f1, highlightbackground="grey", highlightthickness=1)
         f3.pack()
         
-        self.filename = Label(f3, textvariable=self.tileimage, width=100)
+        f3_1 = Frame(f3)
+        f3_1.pack(side=TOP)
+        
+        self.foldername = Label(f3_1, textvariable=self.imagespath, width=100)
+        self.foldername.pack(side=RIGHT)
+        
+        bbtn1 = Button(f3_1, width=20, text="Add picture tiles path...", command=self.add_folder_to_scheme)
+        bbtn1.pack(side=LEFT)
+        
+        f3_2 = Frame(f3)
+        f3_2.pack(side=BOTTOM)
+    
+        self.filename = Label(f3_2, textvariable=self.tileimage, width=100)
         self.filename.pack(side=RIGHT)
         
-        bbtn2 = Button(f3, width=15, text="Tile picture", command=self.add_file_to_scheme)
+        bbtn2 = Button(f3_2, width=20, text="Get mosaic picture", command=self.add_file_to_scheme)
         bbtn2.pack(side=LEFT)
         
-        f4 = Frame(f1)
-        f4.pack(anchor=W)
+        f4 = Frame(f1, highlightbackground="grey", highlightthickness=1)
+        f4.pack(side=LEFT)
+        
+        self.msize = tk.Scale(f4, label="Decrease mosaic image size to-%", orient=tk.HORIZONTAL, length=200)
+        self.msize.pack(anchor=W, padx=10)
+        self.msize.set(80)
         
         self.nc = tk.Scale(f4, label="Number of Clusters", orient=tk.HORIZONTAL, length=200)
         self.nc.pack(anchor=W, padx=10)
         self.nc.set(25)
         
+        self.pixel_n = tk.Scale(f4, label="Tile frame side n pixels", orient=tk.HORIZONTAL, length=200)
+        self.pixel_n.pack(anchor=W, padx=10)
+        self.pixel_n.set(32)
+        
         self.cb1 = Checkbutton(f4, text="Blend with original", variable=self.blend_original, padx=20)
         self.cb1.pack(anchor=W)
         
-        self.cb2 = Checkbutton(f4, text="Closer to cluster centers", variable=self.closer_to_clusters)
-        self.cb2.pack()
+        self.cb2 = Checkbutton(f4, text="Closer to cluster centers", variable=self.closer_to_clusters, padx=20)
+        self.cb2.pack(anchor=W)
         
-        bbtn3 = Button(f4, width=15, text="Create mosaic", command=self.create_mosaic)
+        bbtn3 = Button(f4, width=15, text="Create mosaic", font=fontti, command=self.create_mosaic, height=1)
         bbtn3.pack(anchor=W)
         
-        self.tabs.grid(row=0, column=0)
+        self.f5 = Frame(f1, highlightbackground="grey", highlightthickness=1)
+        self.f5.pack(side=RIGHT, fill="both")
+        
+    def tile_preview(self):
+        global img
+        
+        path = self.imagespath.get()
+        
+        self.clear_frame(self.f5)
+        
+        ns = self.pixel_n.get()
+        tilesize = (ns, ns)
+        
+        with visit_dir(path):
+            filenames = list(os.scandir())
+            filenames = [e.name for e in filenames if ext_check(e.name, EXTS)]
+            np.random.shuffle(filenames)
+            random_names = filenames[:4]
+            random_name = random_names[0]
+            #for random_name in random_names:
+                
+            if True:
+                
+                img = Image.open(random_name)
+                img = img.resize(tilesize)
+                img = img.resize((100,100))
+                
+                tkimage = ImageTk.PhotoImage(img)
+        
+                image_label = tk.Label(self.f5)
+                image_label.config(image=tkimage)
+                image_label.photo = img
+                image_label.pack(fill='both',expand='yes')
+                
+                #break
         
     def add_folder_to_scheme(self):
-        file_name = get_foldername_dialog(self.root, initdir=self.imagespath)
-        self.imagespath.set(file_name)
+        folder_name = get_foldername_dialog(self.root, initdir=self.imagespath)
+        if os.path.exists(folder_name):
+            self.imagespath.set(folder_name)
+            self.tile_preview()
+        else:
+            print("wrong path...",folder_name)
         
     def add_file_to_scheme(self, exts=["jpg","png","bmp","tif"]):
         file_name = get_filename_dialog(self.root, initdir=self.imagespath)
-        if file_name.split(".")[-1] in exts:
+        if ext_check(file_name, exts):
             self.tileimage.set(file_name)
             
     def mosaic_folder(self):
         pass
+    
+    def clear_frame(self, frame):
+        'clear given frame (tk.Frame)'
+        for widget in frame.winfo_children():
+            widget.destroy()
         
     def create_mosaic(self):
-        t = Tiles()
+        
+        ns = self.pixel_n.get()
+        tilesize = (ns, ns)
+        
+        self.t = Tiles(tilesize=tilesize)
+        
         print("initializing images")
         path = self.imagespath.get()
         #pathends = ["one","two","three","four","five"]
         
-        t.get_image_set(path)
+        self.t.get_image_set(path)
             
         path = self.tileimage.get()   
         #path = "C://Users//Mikko//Documents//LiClipse Workspace//firebase//source2//data//xxx//meitsi.jpg"
         
         print("clustering images")
-        t.tile_image(path)
-        t.cluster_up(self.nc.get())
+        
+        rsratio = self.msize.get()/100
+        self.t.get_tile_image(path, resize_ratio=rsratio)
+        self.t.cluster_up(self.nc.get())
         
         print("mosaic creation bitches")
         
-        t.create_mosaic_image(closer_to_clust=[False,True][self.closer_to_clusters.get()],
+        self.t.create_mosaic_image(closer_to_clust=[False,True][self.closer_to_clusters.get()],
                               blend_original=[False,True][self.blend_original.get()],
                               blend_ratio=0.1)
         
         #path = os.path.abspath(__file__)
-        path = os.getcwd()
-        path = path+"//mosaic{}.jpg".format(int(time.time()))
+        #path = os.getcwd()
+        #path = path+"//mosaic{}.jpg".format(int(time.time()))
         
-        to_image(t.mosaic,path)
-        print("done, check working directory")
+        path = Path(os.getcwd())/Path(f"mosaic_{timestamp()}.jpg")
+        
+        to_image(self.t.mosaic, path)
+        print("mosaic done... {}".format(path))
         
 class Tiles:
 
@@ -234,11 +329,9 @@ class Tiles:
             
     def get_image_set(self, path):
         "folder iterating of self.get_pictures_from_folder"
-        for e in os.scandir(path):
-            if e.is_dir():
-                self.get_pictures_from_folder(e.path,False)
-                
-    def get_pictures_from_array(self,array):
+        self.get_pictures_from_folder(path, False)
+
+    def get_pictures_from_array(self, array):
         "get mosaic image set from a np array color image set, for exmaple cifar-10"
         check = True
         for i in range(array.shape[0]):
@@ -254,17 +347,19 @@ class Tiles:
         self.stats[:,0] = [i for i in range(n)]
         self.set_means()
     
-    def get_pictures_from_folder(self, path, clear_previous=True, exts=["jpg","jpeg","png","bmp","tif"]):
+    def get_pictures_from_folder(self, path, clear_previous=True, exts=EXTS):
         "obtains mosaic images from a given folder"
         
         if clear_previous:
             del self.arrays
             self.arrays = []
         
-        paths = [os.path.abspath(x.path) for x in os.scandir(path) if x.path.split(".")[-1] in exts]
+        paths = [os.path.abspath(x.path) for x in os.scandir(path) if ext_check(x.name, EXTS)]
+        
         for path in paths:
-            arr = Image.open(path).resize(self.tilesize)
-            arr = to_array(arr)
+            img = Image.open(path).resize(self.tilesize)
+            arr = to_array(img)
+            
             if arr.shape[2]==3:
                 self.arrays.append(arr)    
     
@@ -275,15 +370,18 @@ class Tiles:
         self.stats[:,0] = [i for i in range(n)]
         self.set_means()
         
-    def get_picture(self, get_filename=True):
+        print("ready...")
+        
+    def get_picture(self, get_filename=True, resize=1.0):
         "appends a single picture to mosaic images from path"
         if get_filename:
             photo_name = get_filename_dialog(self.root)
         else:
-            photo_name = os.getcwd()+"\\"
-            photo_name += self.folder_picture_names[self.picture_index]
+            photo_name = Path(os.getcwd())/Path(self.folder_picture_names[self.picture_index])
+            
+        img = Image.open(photo_name)
 
-        self.pictures.append(Image.open(photo_name))
+        self.pictures.append(img)
     
     def set_means(self):
         "mosaic image array set statistics... used in tiling"
@@ -301,17 +399,21 @@ class Tiles:
             self.stats[i,-1] = distance(self.km.cluster_centers_[pred,:],self.stats[i,1:4])
             i += 1
             
-    def tile_image(self, path, resize=False, resize_ratio=0.50):
+    def get_tile_image(self, path, resize_ratio=1.0):
         "the image to be tiled is set"
         ti = Image.open(path)
-        if resize:
+        
+        if resize_ratio!=1.0:
             rx = int(resize_ratio*ti.size[0])
             ry = int(resize_ratio*ti.size[1])
-            self.tile_image.resize((rx,ry))
+            ti = ti.resize((rx,ry))
         
         self.tile_image = ti
         
+        print("image size: ", self.tile_image.size)
+        
         self.tile_array = to_array(self.tile_image)
+        
         self.shp = self.tile_array.shape
         
     def cluster_up(self, n_clusters=25):
@@ -391,9 +493,6 @@ class Tiles:
         """mosaic tile is given an index representing a single tile from self.arrays"
         mode is deals with how tiles are picked from clusters"""
         
-         #global rows
-         #global r
-        
         w = np.where(self.stats[:,4]==self.cluster_array[i,j])[0]
         rows = self.stats[w,:]
          
@@ -406,7 +505,6 @@ class Tiles:
             return(irow)
          
         if not rows.shape[0]==0:
-
             rows = rows[np.argsort(rows[:, -1])]  
             r = np.random.triangular(0,0,rows.shape[0])
             r = np.round(r)
@@ -424,12 +522,15 @@ def init_gui():
     w = Tk()
     lx,ly = 1000,500
     size = "{}x{}".format(lx,ly)
+    w.title("Mosaic image creator")
     w.geometry(size)
     w.resizable(1, 1) 
     g = Gui1(w)
     w.mainloop()
     
-init_gui()
+    return g
+    
+g = init_gui()
 
 
            
